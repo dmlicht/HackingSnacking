c631706d3ff420a59baf3722ffa2e80f80eb8424
David Lichtenberg <david.m.lichtenberg@gmail.com>
2013-06-27 14:29:55 -0400

    abstracted communication with groupon api into pygroupon module

--- groupon.py
+++ groupon.py
@@ -1,7 +1,6 @@
-import httplib
-import json
 import search_script
 import unicodedata
+import pygroupon
 from keys import keys
 
 BASE_DEAL_URL = 'http://www.groupon.com/deals/'
@@ -10,39 +9,9 @@ def convert_to_ascii(unicode_string):
     """returns the ascii version of string with characters valued over 128 stripped"""
     return unicodedata.normalize("NFKD", unicode_string).encode("ascii", "ignore")
 
-def generate_query_url(base, fields):
-    """takes a base url and set of fields and returns implied url"""
-    field_string = '&'.join([k + '=' + v for (k,v) in fields.items()])
-    return '%s?%s' % (base, field_string)
-
 def main():
-    base_url = "http://api.groupon.com/v2/deals/search"
-    query_url_fields = {
-        "callback": '_',
-        'limit': '4000',
-        'offset': '0',
-        'division_id': 'new-york',
-        'facets': 'category%7Ccategory2',
-        'relevance_context': 'web_local',
-        'show_facets': 'Local',
-        'filters': 'category%3Arestaurants-and-bars',
-        'lat': '40.7143528',
-        'lng': '-74.0059731',
-        'locale': 'en',
-        'secure_assets': 'true',
-        'force_http_success': 'true',
-        '_': '1371507745085',
-        'client_id': keys["GROUPON_CLIENT_ID"]
-    }
-
-    request_string = generate_query_url(base_url, query_url_fields)
-    groupon_connection = httplib.HTTPConnection('api.groupon.com', 80)
-    groupon_connection.request('GET', request_string)
-    response = groupon_connection.getresponse()
-
-    #strip beginning and end characters to expose just the json from the request
-    response_json = json.loads(response.read()[2:-1])
-
+    groupon = pygroupon.Groupon(keys["GROUPON_CLIENT_ID"])
+    response_json = groupon.fetch()
     for deal in response_json['deals']:
         name = deal['merchant']['name']
         ascii_name = convert_to_ascii(name)
---
+++ pygroupon.py
@@ -0,0 +1,45 @@
+import httplib
+import json
+
+DEFAULT_FIELDS = {
+    "callback": '_',
+    'limit': '4000',
+    'offset': '0',
+    'division_id': 'new-york',
+    'facets': 'category%7Ccategory2',
+    'relevance_context': 'web_local',
+    'show_facets': 'Local',
+    'filters': 'category%3Arestaurants-and-bars',
+    'lat': '40.7143528',
+    'lng': '-74.0059731',
+    'locale': 'en',
+    'secure_assets': 'true',
+    'force_http_success': 'true',
+    '_': '1371507745085',
+    'client_id': None
+}
+
+class Groupon(object):
+    """wraps groupon http api for quick use with python
+    by default it will search for all current restaurant deals
+    in the NYC area"""
+
+    def __init__(self, client_id):
+        self._fields = DEFAULT_FIELDS
+        self._fields['client_id'] = client_id
+        self._base_url = "http://api.groupon.com/v2/deals/search"
+
+    def generate_query_url(self):
+        """takes a base url and set of fields and returns implied url"""
+        field_string = '&'.join([k + '=' + v for (k,v) in self._fields.items()])
+        return '%s?%s' % (self._base_url, field_string)
+
+    def fetch(self):
+        """queries groupon api and returns json object with results"""
+        request_string = self.generate_query_url()
+        connection = httplib.HTTPConnection('api.groupon.com', 80)
+        connection.request('GET', request_string)
+        response = connection.getresponse()
+
+        #strip beginning and end characters to expose just the json from the request
+        return json.loads(response.read()[2:-1])
