dd08b1d7d68aba5845ef4f1ad32f80fc3e94e585
David Lichtenberg <david.m.lichtenberg@gmail.com>
2013-08-20 17:07:01 -0400

    recent changes

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from deal_list import Deal
 import cPickle
+import json
 #from flask import session, config, g
 
 DEBUG = True
@@ -11,6 +12,8 @@ app.config.from_object(__name__)
 with open('all_deals') as f:
     deals= cPickle.load(f)
 
+deal_jsons = {deal.name: json.dumps(deal.__dict__) for deal in deals}
+
 # num_non_matching = 0
 # for deal in deals:
 #     if not deal.name_matches():
@@ -18,9 +21,14 @@ with open('all_deals') as f:
 # print num_non_matching + "out of" + len(deals) + "dont match"
 
 # print dir(d[0])
+
 @app.route('/')
 def show_deals():
     return render_template('index.html', deals=deals)
 
+@app.route('/_deal_data')
+def _deal_data():
+    return jsonify(deal_jsons) 
+
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5002)
--- generate_deal_list.py
+++ generate_deal_list.py
@@ -2,9 +2,12 @@ import search_script
 import unicodedata
 import pygroupon
 import cPickle
+import os
 from keys import keys
 
 GROUPON_BASE_DEAL_URL = 'http://www.groupon.com/deals/'
+CURRENT_DIR = '/Users/David/Learning/HackerSchool/hacking-snacking/'
+PICKLE_FILENAME = 'all_deals'
 FILTER_CUTOFF = 4.0
 
 class Deal(object):
@@ -16,21 +19,24 @@ class Deal(object):
             raise Exception
         else:
             name = deal_json['merchant']['name']
+            image_url = deal_json['grid4ImageUrl']
             yelp_score = yelp_json["rating"]
             # name = yelp_json["name"] 
             yelp_url = yelp_json["url"]
             num_reviews = yelp_json["review_count"]
+            #categories = yelp_json['categories']
             loc = yelp_json["location"]
             deal_url = GROUPON_BASE_DEAL_URL + deal_json['id']
-            return cls(name, yelp_score, yelp_url, num_reviews, deal_url, loc, yelp_json, deal_json)
+            return cls(name, yelp_score, yelp_url, num_reviews, deal_url, loc, image_url, yelp_json, deal_json)
 
-    def __init__(self, name, yelp_score, yelp_url, num_reviews, deal_url, address, yelp_json=None, deal_json=None):
+    def __init__(self, name, yelp_score, yelp_url, num_reviews, deal_url, address, image_url=None, yelp_json=None, deal_json=None):
         self.name = name
         self.yelp_score = yelp_score
         self.yelp_url = yelp_url
         self.num_reviews = num_reviews
         self.deal_url = deal_url
         self.address = address
+        self.image_url = image_url
         self.yelp_json = yelp_json
         self.deal_json = deal_json
 
@@ -47,7 +53,10 @@ def main():
     groupon = pygroupon.Groupon(keys["GROUPON_CLIENT_ID"])
     groupon_json = groupon.fetch()
     well_reviewed_deals = []
+    #import pprint
     for deal_json in groupon_json['deals']:
+    #    pprint.pprint(deal_json)
+    #    continue
         name = deal_json['merchant']['name']
         ascii_name = convert_to_ascii(name)
         yelp_json = search_script.get_rating(ascii_name)
@@ -56,6 +65,7 @@ def main():
         deal = Deal.from_json(yelp_json, deal_json)
         if deal.yelp_score >= FILTER_CUTOFF:
             well_reviewed_deals.append(deal)
+    os.chdir(CURRENT_DIR)
     with open('all_deals', 'w') as f:
         cPickle.dump(well_reviewed_deals, f)
 
