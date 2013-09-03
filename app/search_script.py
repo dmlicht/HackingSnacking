a020578b22cc1d18d68edf267cb5956958613aec
David Lichtenberg <david.m.lichtenberg@gmail.com>
2013-09-03 00:02:47 -0400

    pulling less entries from yelp since we only use the first return none if the data pulled from yelp is too contain deal json

--- app/search_script.py
+++ app/search_script.py
@@ -8,12 +8,15 @@ def get_rating(name):
 
     #calls script provided by yelp to access API. 
     #Consumer credentials hardcoded. This seems like it could be bad practice
-    command = 'python search.py --consumer_key="sPXjoKQ5TGRuP8O0VWW_0Q" --consumer_secret="28TsVhiznWyd9BdBiQGOsxKzSSY" --token="zJ6rk5-5Uk0ObRxI-fB7c9vqq7e1iiPb" --token_secret="uCzsZqjbUmOdMoiyyJ55-anlH9U" --term="' + name + '" --location="New York, NY" --limit=5'
+    command = 'python search.py --consumer_key="sPXjoKQ5TGRuP8O0VWW_0Q" --consumer_secret="28TsVhiznWyd9BdBiQGOsxKzSSY" --token="zJ6rk5-5Uk0ObRxI-fB7c9vqq7e1iiPb" --token_secret="uCzsZqjbUmOdMoiyyJ55-anlH9U" --term="' + name + '" --location="New York, NY" --limit=2'
 
     #popen is similar to os.system but returns result string instead of printing to stout
     results = os.popen(command).read()
 
     #parse output. first 3 lines equate to request data and empty line. 4th line is result json
+    if len(results) < 10:
+        return None
+        continue
     url_line, signed_url, _, json_data = str.split(results, '\n', 3)
 
     json_obj = json.loads(json_data)
