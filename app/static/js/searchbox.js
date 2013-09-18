70d1cb9dba312337551fbf7909d21d66e6cd7c39
David Lichtenberg <david.m.lichtenberg@gmail.com>
2013-09-18 11:50:31 -0400

    added searching by categories

--- app/static/js/searchbox.js
+++ app/static/js/searchbox.js
@@ -1,8 +1,3 @@
-// We need to:
-//      * store the json data
-//      * get all stored json elements from try
-//      *
-
 window.onload = function(){
     var xhReq = new XMLHttpRequest();
     xhReq.open("GET", "/_deal_data", false);
@@ -28,21 +23,71 @@ function getDealList(dealsString){
 
 /**
  * takes list of deal objects and returns a trie
- * mapping names of a deal to deal objects
+ * mapping names and tags of deal objects to deals
  */
 function buildTrie(deals){
-    var trie = new this.Trie();
+    var trie = new this.Trie('i'); //i for case insensitive. Borrowed from regex flag
+    tagsDict = createTagToDealListDictionary(deals)
     for (var ii in deals){
-        trie.add(deals[ii]['name'], deals[ii]);
+        trie.add(deals[ii]['name'], [deals[ii]]);
+    }
+    for (var ii in tagsDict){
+        trie.add(ii, tagsDict[ii]);
     }
     return trie;
 }
 
+/**
+ * takes a list of deals and returns a dictionionary
+ * mapping each element of the set of tags associated with deals
+ * to the list of all deals matching the tag
+ */
+function createTagToDealListDictionary(deals){
+    var catDict = new Object();
+    for (var ii in deals){
+        var categories = deals[ii]['categories'];
+        for (var jj in categories){
+            if (!catDict.hasOwnProperty(categories[jj])){
+                catDict[categories[jj]] = new Array();
+            }
+            catDict[categories[jj]].push(deals[ii]);
+        }
+    }
+    return catDict;
+}
+
+
 function filterList(){
 	var query = getQuery();
-    //should trie be non-global? How can I restructure?
+    //NOTE: should trie be non-global? How can I restructure?
     clearElementChildren('deals');
-    populateDealsList(trie.startsWith(query));
+    var listOfLists = trie.startsWith(query);
+    populateDealsList(unionLists(listOfLists));
+}
+
+/**
+ * returns the union of a list of lists
+ * iterates through each list and checks against
+ * a dictionary of seen elements, all elements
+ * are then pushed into a list and returned
+ */
+function unionLists(lists){
+    var unique = new Object();
+    var uniqueArray = new Array();
+    for (var ii in lists){
+        for (var jj in lists[ii]){
+            var name = lists[ii][jj]['name'];
+            //Note: refactor for better implementation than name
+            if (!unique.hasOwnProperty(name)){
+                unique[name] = lists[ii][jj];
+                console.log(name);
+            }
+        }
+    }
+    for (var seen in unique){
+        uniqueArray.push(unique[seen]);
+    }
+    return uniqueArray;
 }
 
 function clearElementChildren(nodeID){
@@ -64,13 +109,14 @@ var populateDealsList = function(deals) {
             deal['name'], 
             deal['yelp_score'],
             deal['num_reviews'],
+            deal['categories'],
             deal['deal_url'],
             deal['yelp_url']
         );
     }
 }
 
-function add_deal(name, score, nreviews, grouponUrl, yelpUrl){
+function add_deal(name, score, nreviews, categories, grouponUrl, yelpUrl){
   var article = document.createElement('article');
   article.className += "deal";
 
@@ -94,6 +140,14 @@ function add_deal(name, score, nreviews, grouponUrl, yelpUrl){
   article.appendChild(div);
   div.appendChild(nameLink);
   div.appendChild(pscore);
+
+  var cats = document.createElement('ul');
+  for (var i = 0; i < categories.length; i++){
+    var el = document.createElement('li');
+    el.innerText = categories[i];
+    cats.appendChild(el)
+  }
+  div.appendChild(cats);
 
   var deals = document.getElementById('deals');
   deals.appendChild(article);
