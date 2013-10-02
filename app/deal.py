5617fbda1aa8dc0cdd1aee2114fc0fbb6b6d936c
David Lichtenberg <david.m.lichtenberg@gmail.com>
2013-10-02 18:21:58 -0400

    added category to deal attribute

--- app/deal.py
+++ app/deal.py
@@ -1,3 +1,4 @@
+import json
 GROUPON_BASE_DEAL_URL = 'http://www.groupon.com/deals/'
 
 class Deal(object):
@@ -16,16 +17,24 @@ class Deal(object):
             num_reviews = yelp_json["review_count"]
             #categories = yelp_json['categories']
             loc = yelp_json["location"]
+            categories = cls.clean_categories(yelp_json["categories"])
             deal_url = GROUPON_BASE_DEAL_URL + deal_json['id']
-            return cls(name, yelp_score, yelp_url, num_reviews, deal_url, loc, image_url, yelp_json, deal_json)
+            return cls(name, yelp_score, yelp_url, num_reviews, deal_url, loc, categories, image_url, yelp_json, deal_json)
 
-    def __init__(self, name, yelp_score, yelp_url, num_reviews, deal_url, address, image_url=None, yelp_json=None, deal_json=None):
+    @staticmethod
+    def clean_categories(categories):
+        """takes yelp formatted categories and returns simple list
+        containing first element of each inner list"""
+        return [cat[0] for cat in categories]
+
+    def __init__(self, name, yelp_score, yelp_url, num_reviews, deal_url, address, categories, image_url=None, yelp_json=None, deal_json=None):
         self.name = name
         self.yelp_score = yelp_score
         self.yelp_url = yelp_url
         self.num_reviews = num_reviews
         self.deal_url = deal_url
         self.address = address
+        self.categories = categories
         self.image_url = image_url
         self.yelp_json = yelp_json
         self.deal_json = deal_json
